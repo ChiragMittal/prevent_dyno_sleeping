a5ea2b80fcaf6ff99f657f16e4c56814487b595b
Chirag Mittal <chirag@aphid.io>
2021-05-10 12:08:02 -0700

    added

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express"); 
+const wakeUpDyno = require("./wokeDyno"); // my module!
+
+
+const PORT = 3000; // whatever port you like
+const DYNO_URL = "https://log-calculator-client.herokuapp.com/";
+const DYNO_URL_1 = "https://log-calculator.herokuapp.com/"; // the url of your dyno
+
+const app = express(); // instantiate Express app
+
+app.listen(PORT, () => {
+    wakeUpDyno(DYNO_URL);
+    wakeUpDyno(DYNO_URL_1); // will start once server starts
+})
