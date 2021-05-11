c227ddb789e39be98783db55867752ec1b338a7d
CHG <chiragmittal97@yahoo.co.in>
2021-05-10 22:55:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const wakeUpDyno = require("./wokeDyno"); // my module!
 
 const PORT = 3000; // whatever port you like
 const DYNO_URL = "https://log-calculator-client.herokuapp.com/";
-const DYNO_URL_1 = "https://log-calculator.herokuapp.com/"; // the url of your dyno
+const DYNO_URL_1 = "https://log-calculator.herokuapp.com/";
+const DYNO_URL_2 = "https://stopdynosleeping.herokuapp.com/"// the url of your dyno
 
 const app = express(); // instantiate Express app
 
 app.listen(PORT, () => {
     wakeUpDyno(DYNO_URL);
-    wakeUpDyno(DYNO_URL_1); // will start once server starts
+    wakeUpDyno(DYNO_URL_1);
+    wakeUpDyno(DYNO_URL_2)// will start once server starts
 })
